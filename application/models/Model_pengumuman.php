ed2d8c2d1e0a3f8a1f9e90f90201d152fff987e7
rachmat jackly <rachmatjackly@streamzinu.com>
2022-01-21 21:07:30 +0700

    add function getAll in Model pengumuman

--- application/models/Model_pengumuman.php
+++ application/models/Model_pengumuman.php
@@ -5,13 +5,7 @@ class Model_pengumuman extends CI_Model {
 
     public function getAll()
     {
-        return $this->db->get('tb_user')->result();
+        return $this->db->get('tb_pengumuman')->result();
     }
 
-    public function getByUsernameAndPassword($username, $password)
-    {
-        $this->db->where('username', $username);
-        $this->db->where('password', $password);
-        return $this->db->get('tb_user')->result();
-    }
 }
