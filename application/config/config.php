9765659f1b22f6fcf789a0d678dadb60f8c9fe36
rachmat jackly <rachmatjackly@streamzinu.com>
2022-01-29 13:28:38 +0700

    configure database, url, CI

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:80/project/';
+$config['base_url'] = 'https://arsipsurat.streamzinu.com/';
 
 /*
 |--------------------------------------------------------------------------
